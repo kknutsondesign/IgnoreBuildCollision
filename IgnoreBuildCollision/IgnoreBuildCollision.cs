dc81df88ea413c4af05c9af7b5fc678e3e0d0ea1
Soggylithe <nivek1892@gmail.com>
2020-10-30 20:29:57 -0500

    Initial Commit
    Ignore all non code or modinfo files

---
+++ IgnoreBuildCollision/IgnoreBuildCollision.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using System.Reflection;
+
+public class IgnoreBuildCollision : Mod
+{
+
+    private Harmony harmonyInstance;
+    public void Start()
+    {
+        MyInput.Keybinds.Add("Alt", new Keybind("alt", KeyCode.LeftAlt, KeyCode.RightAlt));
+
+        harmonyInstance = new Harmony("com.Soggylithe.IgnoreBuildCollision");
+        harmonyInstance.PatchAll();
+
+        
+        Debug.Log("Ignore Build Collision successfully loaded! - Default-AltKey");
+    }
+
+    public void OnModUnload()
+    {
+        MyInput.Keybinds.Remove("Alt");
+
+        harmonyInstance.UnpatchAll();
+        Destroy(gameObject);
+        Debug.Log("Ignore Build Collision has been unloaded!");
+    }
+}
+
+[HarmonyPatch(typeof(BlockCreator), "CanBuildBlock")]
+public class HarmonyPatch_IgnoreCollisionOnAlt
+{
+    [HarmonyPostfix]
+    static BuildError CheckForAlt(BuildError __result)
+    {
+        if (!MyInput.GetButton("Alt"))
+        {
+            return __result;
+        }
+
+        if (__result == BuildError.PositionOccupied || __result == BuildError.PositionOccupied_Same)
+        {
+            return BuildError.None;
+        }
+        return __result;
+    }
+}
