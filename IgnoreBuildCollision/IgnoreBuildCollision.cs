79964a094b1145a2ab42e0a3f176f03ee731778d
Soggylithe <nivek1892@gmail.com>
2020-11-05 00:32:10 -0600

    Added Pipe connectivity through walls
    Changed the GetNearbyPipes method to return list of all neighbor pipes unfiltered.

--- IgnoreBuildCollision/IgnoreBuildCollision.cs
+++ IgnoreBuildCollision/IgnoreBuildCollision.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using HarmonyLib;
+using System.Collections.Generic;
 using System.Reflection;
 
 public class IgnoreBuildCollision : Mod
@@ -21,7 +22,7 @@ public class IgnoreBuildCollision : Mod
     {
         MyInput.Keybinds.Remove("Alt");
 
-        harmonyInstance.UnpatchAll();
+        harmonyInstance.UnpatchAll("com.Soggylithe.IgnoreBuildCollision");
         Destroy(gameObject);
         Debug.Log("Ignore Build Collision has been unloaded!");
     }
@@ -43,5 +44,28 @@ public class HarmonyPatch_IgnoreCollisionOnAlt
             return BuildError.None;
         }
         return __result;
+    }
+}
+
+[HarmonyPatch(typeof(Pipe), "GetNearbyPipes")]
+public class HarmonyPatch_ConnectPipesThroughWalls
+{
+    [HarmonyPostfix]
+    static void IgnoreWalls(ref List<Pipe> __result, Pipe ___pipe, Pipe ___pipe2, Pipe ___pipe3, Pipe ___pipe4, Pipe ___pipe5, Pipe ___pipe6)
+    {
+        List<Pipe> list = new List<Pipe>();
+
+        list.AddUniqueOnly(___pipe);
+        list.AddUniqueOnly(___pipe2);
+        list.AddUniqueOnly(___pipe3);
+        list.AddUniqueOnly(___pipe4);
+        list.AddUniqueOnly(___pipe5);
+        list.AddUniqueOnly(___pipe6);
+
+        if (!list.ContainsItems<Pipe>())
+        {
+            __result = null;
+        }
+        else { __result = list; }
     }
 }
